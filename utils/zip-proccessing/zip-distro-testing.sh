03b256d1b4a9cdea464a4695b2925c70d64bd899
Ondrej Chaloupka <ochaloup@redhat.com>
2012-07-16 14:06:41 +0200

    zip-distro-testing - version 1.0

--- utils/zip-proccessing/zip-distro-testing.sh
+++ utils/zip-proccessing/zip-distro-testing.sh
@@ -14,12 +14,15 @@ CLASSPATH_ADD=
 EXIT_CODE=0
 # go to debug mode
 IS_DEBUG=
+# quiet mode
+IS_QUIET=
 
 # Declaration of constants
 TATTLETALE_REPORT_DIR_NAME="tattletale_reports"
 TATTLETALE_SCRIPT="tattletale.groovy"
 SHARE_DIR_NAME="share"
 TOMCAT_DIR_NAME_REGEXP="tomcat-"
+UNZIPED_JAR_DIR_SUFFIX=".unzippedjar"
 SCRIPT_PATH="$0"
 SCRIPT_DIR=${0%\/*}
 # Declaration of global variables
@@ -29,7 +32,14 @@ declare -a INPUT_DIRS
 ######################## FUNCTIONS ########################
 function debug() {
   if [ "x$IS_DEBUG" != "x" ]; then
-  	echo "+ $1"
+  	eecho "+ $1"
+  fi
+}
+
+# echo function which respects IS_QUIET flag
+function eecho() {
+ if [ "x$IS_QUIET" == "x" ]; then
+    echo "$1"
   fi
 }
 
@@ -38,7 +48,7 @@ function just_name() {
   local RESULT="${FILENAME%.*}"
 
   if [ "x$2" == "x" ]; then
-  	echo "$RESULT"
+  	eecho "$RESULT"
   else
     eval $2="'$RESULT'"
   fi
@@ -51,10 +61,10 @@ function unzip_with_dir() {
   local OUT_ZIP_DIR="${2}/${FILENAME_WITHOUT_EXT}"
   mkdir "$OUT_ZIP_DIR"
   #if [ $? -ne 0 ]; then
-  #	echo "Folder $OUT_ZIP_DIR can't be created and zip file $1 can't be unzipped. Exiting..."
+  #	eecho "Folder $OUT_ZIP_DIR can't be created and zip file $1 can't be unzipped. Exiting..."
   #	exit 2
   #fi
-	echo "Unzipping $1 to $OUT_ZIP_DIR"
+	eecho "Unzipping $1 to $OUT_ZIP_DIR"
 	unzip -oq "$1" -d "$OUT_ZIP_DIR"
 	
 	# returning value with passing value to the variable name in last arg 
@@ -65,11 +75,12 @@ function unzip_with_dir() {
 
 # wget_all_linked_zip web_page_link output_dir result_var_name
 # get all zip files from the passed webpage name
+# param1: http link to a page with list of zip files
 function wget_all_linked_zip() {
 	local TO_DOWN=`echo "$1" | sed "s/^\(.*\)[/]$/\1/"` # strip off the last slash in the address
 	wget -qO /dev/null "$1" # check existence of the page
 	if [ $? -ne 0 ]; then
-		echo "Web page '$1' is not available. Exiting..."
+		eecho "Web page '$1' is not available. Exiting..."
 		exit 2
 	fi
   wget -qO - "$TO_DOWN" | grep -ioP "<a\b[^<>]*?\b(href=\s*(?:\"[^\"]*\"|'[^']*'|\S+))" |\
@@ -81,15 +92,37 @@ function wget_all_linked_zip() {
 }
 
 # is the parameter a jar
+# param1: name of file with path that will be tested to be jar
 is_jar() {
-  JAR_NAME="$1"
-  EXTENSION=`echo ${JAR_NAME##*.} | tr '[:upper:]' '[:lower:]'`
+  local JAR_NAME="$1"
+  local EXTENSION=`echo ${JAR_NAME##*.} | tr '[:upper:]' '[:lower:]'`
   file --brief "$JAR_NAME" | grep -iq 'zip archive'
   if [ $? -eq 0 -a -s "$JAR_NAME" -a "$EXTENSION" = 'jar' ]; then
     return 0;
   else
     return 1
   fi
+}
+
+# calculate md5checksum on the dir recursively in case of jar files
+# param1: dir to process
+# param2: file where the checksums will be added
+calculate_md5checksums() {
+  local DIR_TO_PROCESS="$1"
+  local MD5_REPORT_FILE="$2"
+  debug "Calculating md5 check sums for $DIR_TO_PROCESS"  
+  
+  find "$DIR_TO_PROCESS" -name '*' | while read I; do
+  	if [ -f "$I" ]; then
+      md5sum "$I" >> "$MD5_REPORT_FILE"
+      is_jar "$I"
+      if [ $? -eq 0 ]; then # on jar recursively going down
+      	local DIR_TO_UNZIP="${I}${UNZIPED_JAR_DIR_SUFFIX}"
+        unzip -oq "$I" -d "$DIR_TO_UNZIP"
+        calculate_md5checksums "$DIR_TO_UNZIP" "$MD5_REPORT_FILE"
+      fi
+    fi
+  done
 }
 
 
@@ -119,6 +152,9 @@ while [ $# -gt 0 ]; do
       IS_DEBUG=1
       set -x
       ;;
+    -q | -quiet | --quiet)
+      IS_QUIET=1
+      ;;
       
 
     -h | --help)
@@ -128,6 +164,7 @@ while [ $# -gt 0 ]; do
       echo -e "-o or --output dir           output directory"
       echo -e "-cp or --classpath classes   list of classes split by colon sing as it is normal in Linux."\
            "This Argument will be added as argument '-cp' of java programs."
+      echo -e "-q or --quiet                quiet - no output messages please"
       echo -e "-d or --debug                debug mode on"
       echo -e "-h or --help                 will show this help"           
       exit $EXIT_CODE
@@ -159,7 +196,7 @@ done
 
 debug "Arguments [$#]: $@" #DEBUG
 if [ $# -lt 1 ]; then
-	echo "The script needs parameter where it can find the zip files."
+	eecho "The script needs parameter where it can find the zip files."
 	exit 1
 fi
 
@@ -176,14 +213,14 @@ else
   	# Download web page
   	if [ ! -d "$LOOP_ITEM" -a ! -s "$LOOP_ITEM" ]; then
 	  	# where zip could be downloaded
-			DIR_TO_DOWNLOAD_ZIPS="$OUTPUT_DIR/$DOWNLOAD_DIR_NAME"
-			if [ ! -d "$DIR_TO_DOWNLOAD_ZIPS" ]; then
-			  mkdir -p "$DIR_TO_DOWNLOAD_ZIPS"
-			fi
-  		# wget_all_linked_zip "$LOOP_ITEM" "$DIR_TO_DOWNLOAD_ZIPS"
-  		LOOP_ITEM="$DIR_TO_DOWNLOAD_ZIPS"
-  	fi
-  	debug "Processing item from arguments (already processed by web page routine): $LOOP_ITEM"
+		DIR_TO_DOWNLOAD_ZIPS="$OUTPUT_DIR/$DOWNLOAD_DIR_NAME"
+        if [ ! -d "$DIR_TO_DOWNLOAD_ZIPS" ]; then
+          mkdir -p "$DIR_TO_DOWNLOAD_ZIPS"
+        fi
+        wget_all_linked_zip "$LOOP_ITEM" "$DIR_TO_DOWNLOAD_ZIPS"
+        LOOP_ITEM="$DIR_TO_DOWNLOAD_ZIPS"
+    fi
+    debug "Processing item from arguments (already processed by web page routine): $LOOP_ITEM"
   	
     # Getting list of unzipped directories
     if [ -d "$LOOP_ITEM" ]; then # directory - unzip all zip files
@@ -197,7 +234,7 @@ else
       debug "Returned unzipped dir $UNZIPPED_DIR" #DEBUG
       INPUT_DIRS=("$UNZIPPED_DIR")
     else 
-      echo "The item to process ($LOOP_ITEM) is neither file nor directory. Exiting..."
+      eecho "The item to process ($LOOP_ITEM) is neither file nor directory. Exiting..."
       exit 3   
     fi
     
@@ -206,7 +243,7 @@ fi
 
 # Do we have something to do?
 if [ ${#INPUT_DIRS[@]} -lt 1 ]; then
-  echo "No files found to process. Exiting..."
+  eecho "No files found to process. Exiting..."
   exit 0 
 fi
 
@@ -217,28 +254,28 @@ rm -rf "$TATTLETALE_REPORT_DIR"/*
 
 # Process with reports
 for DIR_TO_PROCESS in "${INPUT_DIRS[@]}"; do
-  debug "Processing reports for $DIR_TO_PROCESS" 
   DIR_TO_PROCESS_BASENAME=`basename "${DIR_TO_PROCESS}"`
   
   # Tattletale
+  eecho "Processing tattletale report for $DIR_TO_PROCESS"
   TATTLETE_OUTPUT="${TATTLETALE_REPORT_DIR}/${DIR_TO_PROCESS_BASENAME}"
   mkdir -p "$TATTLETE_OUTPUT" 
   PATH_TO_SHARE_DIR=`find "$DIR_TO_PROCESS" -type d -name "$SHARE_DIR_NAME"`
   ls "$PATH_TO_SHARE_DIR" | grep "$TOMCAT_DIR_NAME_REGEXP" | while read TOMCAT_DIR_NAME; do
     TOMCAT_DIR="${PATH_TO_SHARE_DIR}/${TOMCAT_DIR_NAME}"
-    # groovy -Doutput="$TATTLETE_OUTPUT" -Dtestdir="$TOMCAT_DIR" "${SCRIPT_DIR}/${TATTLETALE_SCRIPT}"
+    groovy -Doutput="$TATTLETE_OUTPUT" -Dtestdir="$TOMCAT_DIR" "${SCRIPT_DIR}/${TATTLETALE_SCRIPT}"
   done
-  echo "Tattletale report created for $DIR_TO_PROCESS. Output placed in $TATTLETE_OUTPUT"
+  eecho "Tattletale report created for $DIR_TO_PROCESS. Output placed in $TATTLETE_OUTPUT"
   
   # MD5 checksums
+  eecho "Calculating checksum for $DIR_TO_PROCESS"
   MD5_REPORT="${OUTPUT_DIR}/${DIR_TO_PROCESS_BASENAME}.md5"
-  rm -rf "$MD5_REPORT" 
-  find "$DIR_TO_PROCESS" -name '*' | while read I; do
-  	[ -f "$I" ] && md5sum "$I" >> "$MD5_REPORT"
-  done
-  sort -k2 "$MD5_REPORT" > "$MD5_REPORT.tmp"
+  rm -rf "$MD5_REPORT"
+  calculate_md5checksums "$DIR_TO_PROCESS" "$MD5_REPORT"
+  debug "Deleting temporarily unzipped jar files with suffix $UNZIPED_JAR_DIR_SUFFIX from $DIR_TO_PROCESS"
+  find "$DIR_TO_PROCESS" -name "*${UNZIPED_JAR_DIR_SUFFIX}" | xargs rm -rf 
+  debug "Sorting $MD5_REPORT..."
+  sort -k2 "$MD5_REPORT" | uniq | > "$MD5_REPORT.tmp"
   mv -f "$MD5_REPORT.tmp" "$MD5_REPORT"
-  echo "MD5 checksum report created in $MD5_REPORT."
-  
-  # unzip -q "${DIR}/${1}" -d "$TMP_DEST_DIR"
+  eecho "MD5 checksum report created in $MD5_REPORT."
 done
